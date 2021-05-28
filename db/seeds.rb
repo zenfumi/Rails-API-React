ccb6d4a1c0dcd1b203ac8e95070fae01c6443488
zenfumi <ys00878@gmail.com>
2021-05-29 00:08:12 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+  restaurant = Restaurant.new(
+    name: "testレストラン_#{n}",
+    fee: 100,
+    time_required: 10,
+  )
+
+  12.times do |m|
+    restaurant.foods.build(
+      name: "フード名_#{m}",
+      price: 500,
+      description: "フード_#{m}の説明文です。"
+    )
+  end
+
+  restaurant.save!
+end
