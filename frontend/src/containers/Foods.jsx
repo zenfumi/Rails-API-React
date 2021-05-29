16afa2f62544154e1f89dc0f2c2876c931df02dc
zenfumi <ys00878@gmail.com>
2021-05-29 16:27:51 +0900

    fix

---
+++ frontend/src/containers/Foods.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+export const Foods = ({ match }) => {
+  return (
+    <Fragment>
+      フード一覧
+      <p>restaurantsIdは {match.params.restaurantsId} です</p>
+    </Fragment>
+  );
+};
---
+++ frontend/src/containers/Restaurants.jsx
@@ -0,0 +1,5 @@
+import React, { Fragment } from "react";
+
+export const Restaurants = () => {
+  return <Fragment>レストラン一覧</Fragment>;
+};
